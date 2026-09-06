ab07542ac6cd84c4d633498c4e25fb96c1627311
SunnyMinh <131391897+SunnyMinh@users.noreply.github.com>
2026-09-06 17:33:10 +0700

    Delete_non_using

--- src/mqtt/mqtt.module.ts
+++ src/mqtt/mqtt.module.ts
@@ -2,16 +2,11 @@ import { Module } from '@nestjs/common';
 import { MonitoringModule } from 'src/monitoring/monitoring.module';
 import { ThresholdModule } from 'src/threshold/threshold.module';
 import { MqttService } from './mqtt.service';
-import {
-  DeviceController,
-} from './device.controller';
+
 @Module({
   imports: [
     MonitoringModule,
     ThresholdModule,
-  ],
-  controllers: [
-    DeviceController,
   ],
   providers: [
     MqttService,
